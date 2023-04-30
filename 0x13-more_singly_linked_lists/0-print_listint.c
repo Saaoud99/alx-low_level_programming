d8eb0a217fb7e315c3a0d6540b16aaa217030c54
saaoud99 <saaoud.abdelhadi@gmail.com>
2023-04-30 09:37:45 -0700

    yes i can

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,23 +1,22 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_listint - prints all the elements of a listint_t list.
- *
- * @h: head of linklist node
- *
+ * print_list - a function that prints
+ * all elements of a list - listint_t
+ * @h: head list pointer
  * Return: the number of nodes
- *
  */
+
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t i = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
+
 		h = h->next;
-		count++;
+		i++;
 	}
-	return (count);
+	return (i);
 }
